82ff8c51ef07688e0e0c17f731283d60040f9dd7
Anwar Aakash <aakaashhossain@gmail.com>
2021-08-13 23:17:45 +0600

    generate prisma ,migration and configure nodemon

--- app.ts
+++ app.ts
@@ -1,7 +1,15 @@
 import express, { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const app = express();
 const PORT = 5000 || process.env.PORT;
+
+app.get("/user/all", (req: Request, res: Response) => {
+  const users = prisma.user.findMany();
+  return res.json(users);
+});
 
 app.get("/", (req: Request, res: Response) => {
   return res.json("hello");
